8e1e40ce979f879efbd41f6ba602a1873c285e91
Jillian Schleicher <jmschl@uw.edu>
2018-02-05 17:10:08 -0800

    played with test case, added function to calculate averages rather than separate lines

--- Side_Projects/bead_patterns.py
+++ Side_Projects/bead_patterns.py
@@ -96,39 +96,46 @@ plt.colorbar()
 ax4 = fig.add_subplot(224)  # bottom right
 ax4 = plt.imshow(final_result, interpolation='nearest', aspect=bead_aspect)
 
-#
-#def pixelate(original, new):
-#    
-#    new = original / 3
-#    
-#    return new
-#
-#pixelate(o_reds, new_reds)
+
 
 
 # Simple case to test out
-#test_reds = [[82, 0, 82, 82],[82, 82, 82, 79],
-#             [82, 82, 82, 47],[82, 90, 82, 82]]
-#test_greens = [[179, 0, 136, 136],[179, 39, 179,180],
-#               [179, 179, 179, 229],[179, 200, 179, 179]]
-#test_blues = [[243, 0, 44, 44],[243, 243, 243, 52],
-#              [243, 243, 102, 47],[243, 39, 243, 243]]
-#test_result = np.stack([test_reds, test_greens, test_blues], 
-#                       axis=2).astype(np.uint8)
-##test_result = np.dtype=np.uint8
-##shape=(2,2), dtype=float
-#fig = plt.figure()
-#ax1 = fig.add_subplot(221)  # top left
-#ax1 = plt.imshow(test_reds,cmap='Reds',interpolation='nearest')
+test_reds = np.array([[82, 0, 82, 82],[82, 82, 82, 79],
+             [82, 82, 82, 47],[82, 90, 82, 82]])
+test_greens = np.array([[179, 0, 136, 136],[179, 39, 179,180],
+               [179, 179, 179, 229],[179, 200, 179, 179]])
+test_blues = np.array([[243, 0, 44, 44],[243, 243, 243, 52],
+              [243, 243, 102, 47],[243, 39, 243, 243]])
+
+def pixelate(original, rows, columns, r_calc, c_calc):
+    
+    # reshape works as follows:
+    # (final # rows, rows to calculate final rows, final # columns, 
+    #                                      columns to calculate final columns)
+    new = original.reshape(rows,r_calc,columns,c_calc)
+    
+    color_result = new.mean(axis=3).mean(axis=1)
+    
+    return color_result
+
+r_result = pixelate(test_reds, 1, 2, 4, 2)      # Alternates: 
+g_result = pixelate(test_greens, 1, 2, 4, 2)    # (2,2,2,2), (1,1,4,4)
+b_result = pixelate(test_blues, 1, 2, 4, 2)
+
+test_result = np.stack([r_result, g_result, b_result], axis=2).astype(np.uint8)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(221)  # top left
+ax1 = plt.imshow(test_reds,cmap='Reds',interpolation='nearest')
+plt.colorbar()
+ax2 = fig.add_subplot(222)  # top right
+ax2 = plt.imshow(test_greens, cmap='Greens', interpolation="nearest")
+plt.colorbar()
+ax3 = fig.add_subplot(223)  # bottom left
+ax3 = plt.imshow(test_blues, cmap='Blues', interpolation="nearest")
+plt.colorbar()
+ax4 = fig.add_subplot(224)  # bottom right
+ax4 = plt.imshow(test_result, interpolation="nearest")
 #plt.colorbar()
-#ax2 = fig.add_subplot(222)  # top right
-#ax2 = plt.imshow(test_greens, cmap='Greens', interpolation="nearest")
-#plt.colorbar()
-#ax3 = fig.add_subplot(223)  # bottom left
-#ax3 = plt.imshow(test_blues, cmap='Blues', interpolation="nearest")
-#plt.colorbar()
-#ax4 = fig.add_subplot(224)  # bottom right
-#ax4 = plt.imshow(test_result, interpolation="nearest")
-##plt.colorbar()
     
 
